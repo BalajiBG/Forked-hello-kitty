47e39d14def185f639303cb9863269bff0696f30
stackgen-automation <automation@stackgen.com>
2024-09-28 10:21:43 +0000

    Automated commit

--- DevInfraTargetPath/terraform/outputs.tf
+++ DevInfraTargetPath/terraform/outputs.tf
@@ -1,28 +1,4 @@
 
-output "aws_s3_balaji_bg_arn" {
-  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.arn
-  sensitive = false
-}
-
-
-output "aws_s3_balaji_bg_bucket_name" {
-  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_name
-  sensitive = false
-}
-
-
-output "aws_s3_balaji_bg_bucket_website_endpoint" {
-  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_website_endpoint
-  sensitive = false
-}
-
-
-output "aws_s3_balaji_bg_kms_arn" {
-  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.kms_arn
-  sensitive = false
-}
-
-
 output "aws_lambda_forked_hello_kitty_function_balaji_bg_function_arn" {
   value = module.appcd_9dc9b835-b99e-5b13-836b-d208d30c92ec.function_arn
   sensitive = false
@@ -59,13 +35,37 @@ output "aws_iam_role_forked_hello_kitty_role_balaji_bg2_name" {
 }
 
 
+output "aws_cloudwatch_log_group_aws_lambda_forked_hello_kitty_function_balaji_bg_arn" {
+  value = module.appcd_72b072fa-0351-56ad-947f-0b584f59cc69.arn
+  sensitive = false
+}
+
+
 output "aws_cloudwatch_log_group_aws_lambda_forked_hello_kitty_function_balaji_bg_name" {
   value = module.appcd_72b072fa-0351-56ad-947f-0b584f59cc69.name
   sensitive = false
 }
 
 
-output "aws_cloudwatch_log_group_aws_lambda_forked_hello_kitty_function_balaji_bg_arn" {
-  value = module.appcd_72b072fa-0351-56ad-947f-0b584f59cc69.arn
+output "aws_s3_balaji_bg_arn" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.arn
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_bucket_name" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_name
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_bucket_website_endpoint" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_kms_arn" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.kms_arn
   sensitive = false
 }

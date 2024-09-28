e6d774bb6a87c9c42495df287535e6cb1fb65558
stackgen-automation <automation@stackgen.com>
2024-09-28 09:47:25 +0000

    Automated commit

---
+++ DevInfraTargetPath/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with appCD.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/eb61ee67-fd42-4a12-b53a-ec3eae2e4018)
---
+++ DevInfraTargetPath/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_iam_role_forked_hello_kitty_role_balaji_bg_arn" {
+  value = module.appcd_9dc3160d-75cf-5aca-ae0c-732652a71bc1.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_forked_hello_kitty_role_balaji_bg_name" {
+  value = module.appcd_9dc3160d-75cf-5aca-ae0c-732652a71bc1.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_forked_hello_kitty_function_balaji_bg_arn" {
+  value = module.appcd_72b072fa-0351-56ad-947f-0b584f59cc69.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_forked_hello_kitty_function_balaji_bg_name" {
+  value = module.appcd_72b072fa-0351-56ad-947f-0b584f59cc69.name
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_arn" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.arn
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_bucket_name" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_name
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_bucket_website_endpoint" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_balaji_bg_kms_arn" {
+  value = module.appcd_7742ac21-8632-5e41-9598-82b4152bd845.kms_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_forked_hello_kitty_function_balaji_bg_function_arn" {
+  value = module.appcd_9dc9b835-b99e-5b13-836b-d208d30c92ec.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_forked_hello_kitty_function_balaji_bg_function_name" {
+  value = module.appcd_9dc9b835-b99e-5b13-836b-d208d30c92ec.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_forked_hello_kitty_function_balaji_bg_function_url" {
+  value = module.appcd_9dc9b835-b99e-5b13-836b-d208d30c92ec.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_forked_hello_kitty_function_balaji_bg_invoke_arn" {
+  value = module.appcd_9dc9b835-b99e-5b13-836b-d208d30c92ec.invoke_arn
+  sensitive = false
+}
